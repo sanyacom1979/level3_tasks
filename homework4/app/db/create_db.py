5722e7ea7aaad3b9777a6447f98b836fc8f6fac6
Alex <sanyacomgm@gmail.com>
2023-03-28 09:50:31 +0300

    Add lesson 13

--- homework4/app/db/create_db.py
+++ homework4/app/db/create_db.py
@@ -1,5 +1,5 @@
 from db.base import engine, Base
-from db.models import User
+from db.models import CityDistance
 
 Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine, tables=[CityDistance.__table__])
